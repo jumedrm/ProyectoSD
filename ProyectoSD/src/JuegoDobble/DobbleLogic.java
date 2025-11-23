cc6b65e6af56149f181c8f3e597c2ad6616162ca
Juan Luis Medrano Miguel <jumedrm@unirioja.es>
2025-11-23 20:19:52 +0100

    no se sube el código, solo las clases y no sé por qué

--- ProyectoSD/src/JuegoDobble/DobbleLogic.java
+++ ProyectoSD/src/JuegoDobble/DobbleLogic.java
@@ -1,5 +1,51 @@
 package JuegoDobble;
 
-public class DobbleLogic {
 
+import java.util.*;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class DobbleLogic {
+ private List<List<Integer>> mazo; 
+
+ public DobbleLogic() {
+     this.mazo = new ArrayList<>();
+     
+     
+     // 7 cartas, 4 símbolos, prueeeebaaaaa
+     mazo.add(Arrays.asList(1, 2, 3, 4));    
+     mazo.add(Arrays.asList(1, 5, 6, 7));    
+     mazo.add(Arrays.asList(2, 5, 8, 9));    
+     mazo.add(Arrays.asList(3, 6, 8, 10));   
+     mazo.add(Arrays.asList(4, 7, 9, 11));   
+     mazo.add(Arrays.asList(1, 8, 11, 12));  
+     mazo.add(Arrays.asList(2, 6, 11, 13));  
+     
+ }
+
+ //Saca y devuelve la siguiente carta del mazo
+ public List<Integer> repartirCarta() {
+     if (!mazo.isEmpty()) {
+         return mazo.remove(0);
+     }
+     // deeevuelve null si no hay más cartas.
+     return null;
+ }
+
+
+  //Verifica que el símbolo seleccionado esté en ambas cartas
+ public boolean esCoincidenciaValida(int simbolo, List<Integer> cartaJugador, List<Integer> cartaCentral) {
+     if (cartaJugador == null || cartaCentral == null) {
+         return false;
+     }
+
+     
+     boolean enJugador = cartaJugador.contains(simbolo);
+     boolean enCentral = cartaCentral.contains(simbolo);
+     
+     return enJugador && enCentral; 
+ }
 }
--- ProyectoSD/src/JuegoDobble/DobbleServer.java
+++ ProyectoSD/src/JuegoDobble/DobbleServer.java
@@ -1,5 +1,27 @@
 package JuegoDobble;
 
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
 public class DobbleServer {
+    private static final int PUERTO = 12345;
+   
+
+    public static void main(String[] args) {
+        System.out.println("Servidor Dobble iniciando...");
+        try (ServerSocket serverSocket = new ServerSocket(PUERTO)) {
+            System.out.println("Servidor escuchando en el puerto " + PUERTO);
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Cliente conectado desde: " + clientSocket.getInetAddress());
+
+               
+                
+            }
+        } catch (IOException e) {
+            System.out.println("Error en el servidor principal: " + e.getMessage());
+        }
+    }
 
 }
